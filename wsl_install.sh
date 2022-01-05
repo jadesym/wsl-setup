034cead7825478f6e9d9c7586b260afe54b71958
Kevin Fu <kevin.fu808@gmail.com>
2022-01-04 23:06:44 -0800

    Adding wsl installation script and README

---
+++ wsl_install.sh
@@ -0,0 +1,10 @@
+sudo apt update && sudo apt upgrade
+
+mkdir workspace
+cd workspace
+
+# Install Github CLI for Ubuntu
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
