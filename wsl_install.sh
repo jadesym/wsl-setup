f29cfac6a01c03d2850e05cfa99f28b215b3af8e
Kevin Fu <kevin.fu808@gmail.com>
2022-01-16 22:58:01 -0800

    Adding yes for apt installs

--- wsl_install.sh
+++ wsl_install.sh
@@ -3,7 +3,7 @@ sudo apt update && sudo apt upgrade
 # Update to the latest version of git
 sudo apt-add-repository ppa:git-core/ppa
 sudo apt-get update
-sudo apt-get install git
+sudo apt-get -y install git
 
 # Set default branch for git to use main
 git config --global init.defaultBranch main
@@ -16,4 +16,4 @@ cd workspace
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
-sudo apt install gh
+sudo apt -y install gh
